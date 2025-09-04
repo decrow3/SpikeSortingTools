c18f71f11651476675496e3d96cd0ff3d5708a7d
decrow3 <decrow3@gmail.com>
2025-09-04 05:53:44 -0700

    save out datafiltering

--- SpikeGLX_ext_ref_2025.py
+++ SpikeGLX_ext_ref_2025.py
@@ -8,7 +8,7 @@ import gc
 import spikeinterface.full as si
 
 #%% Change this code to load your data
-data_dir=   r"/mnt/NPX/Luke/LukeV1-20240717/Luke0717_V1_g0/"
+data_dir=   r"/mnt/NPX/Luke/20250805/Luke0805_V2V1_g0/"
 
 stream_id = "imec0.ap" #usually imec0 is first inserted probe (often V2/MT), imec1 is second probe (often V1)
 seg = si.read_spikeglx(folder_path=data_dir, load_sync_channel=False, stream_id=stream_id)# experiment_names="experiment1")
@@ -20,7 +20,10 @@ seg = si.read_spikeglx(folder_path=data_dir, load_sync_channel=False, stream_id=
 
 #%%
 # run pipelines
-pipeline_dir = Path('/home/huklab/Documents/RyanSorting/SpikeSortingTools/pipeline_results_Luke0717_V1_g0_imec0_thresh1_tst')
+# last part of data_dir = data_dir.split('/')[-2] # get the last part of the data_dir, this is the experiment name
+sess_name = data_dir.split('/')[-2]  # get the last part of the data_dir, this is the experiment name
+stream_name = stream_id.split('.')[0] # get the stream id without the extension
+pipeline_dir = Path(f'/home/huklab/Documents/RyanSorting/SpikeSortingTools/pipeline_results_{sess_name}_{stream_name}')
 pipeline_dir.mkdir(parents=True, exist_ok=True)
 
 #%%
@@ -46,14 +49,16 @@ seg_pre = condition_signal(seg, cache_dir=pipeline_dir / 'conditioning', noise_t
 # cur_results = run_cur(seg_saved, ks4_sorter, ks4_results, pipeline_dir / 'cur', recalc=False) # this should save out some merges
 
 #%% Motion issue on SpikeGLX, this may have had more to do with the conditioning failing, kilosort4 is actually more robust??
-seg_motion = correct_motion(seg_pre, cache_dir=pipeline_dir / 'motion', recalc=False, method='med')
-plot_motion_output(seg_motion, cache_dir=pipeline_dir / 'motion')
+#seg_motion = correct_motion(seg_pre, cache_dir=pipeline_dir / 'motion', recalc=False, method='med')
+#plot_motion_output(seg_motion, cache_dir=pipeline_dir / 'motion')
 
+# skipping motion correction, just running it in kilosort
+seg_motion = seg_pre
 
 #%% Kilosort4 parameters
 # OpenEphys
 sorter_params = get_default_sorter_params('kilosort4')
-sorter_params['do_correction'] = False # Turns off drift correction
+sorter_params['do_correction'] = True # Turns off drift correction
 sorter_params['save_extra_vars'] = True # required for truncation qc
 sorter_params['Th_universal'] = 9
 sorter_params['Th_learned'] = 8
@@ -93,6 +98,67 @@ except Exception as e:
 
 print(f'Finished processing')
 
-#%%
+#%% Saving out to matlab files
+import numpy as np
+import os
+
+qc_outdir       = pipeline_dir / 'qc'
+waveformsfile   ='waveforms/waveforms.npz'
+refractoryfile  ='refractory/refractory_qc.npz'
+truncation      ='amp_truncation/truncation_qc.npz'
+
+presencefile    ='amp_truncation/present_qc.npz'
 
 
+def load_qc_data(qc_outdir, filename):
+    filepath = Path(qc_outdir) / filename
+    if not filepath.exists():
+        raise FileNotFoundError(f"File {filepath} does not exist.")
+    
+    try:
+        data = np.load(filepath, allow_pickle=True)
+        return data
+    except Exception as e:
+        raise RuntimeError(f"Failed to load {filepath}: {e}")
+
+# Load the data
+waveforms_data = load_qc_data(qc_outdir, waveformsfile)
+refractory_data = load_qc_data(qc_outdir, refractoryfile)
+truncation_data = load_qc_data(qc_outdir, truncation)
+presence_data = load_qc_data(qc_outdir, presencefile)
+
+#Saving out the data to matlab compatible mat files
+import scipy.io as sio
+def save_to_mat(data, filename):
+    """Save numpy data to a .mat file."""
+    try:
+        sio.savemat(filename, data)
+        print(f"Data saved to {filename}")
+    except Exception as e:
+        raise RuntimeError(f"Failed to save data to {filename}: {e}")
+# Define output filenames
+output_waveforms_file = os.path.join(qc_outdir, 'waveforms_data.mat')
+output_refractory_file = os.path.join(qc_outdir, 'refractory_data.mat')
+output_truncation_file = os.path.join(qc_outdir, 'truncation_data.mat')
+output_presence_file = os.path.join(qc_outdir, 'presence_data.mat')
+# Save the data to .mat files
+save_to_mat(waveforms_data, output_waveforms_file)
+save_to_mat(refractory_data, output_refractory_file)
+save_to_mat(truncation_data, output_truncation_file)
+save_to_mat(presence_data, output_presence_file)
+
+
+import glob
+npzFiles = glob.glob("pipeline_dir / 'cur' /cur_sorter_output/ops.npy")
+
+for f in npzFiles:
+    fm = os.path.splitext(f)[0]+'.mat'
+    d = np.load(f,allow_pickle=True)
+    xc=d.item()['xc']
+    yc=d.item()['yc']
+    matout={"xc":xc,"yc":yc}
+    save_to_mat(fm, matout)
+    print('generated ', fm, 'from', f)
+
+# Print confirmation of saved files
+print("All data has been saved successfully.")
